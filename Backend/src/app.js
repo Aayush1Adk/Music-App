2b835854c4c7828e6e3c8d68a226e4158f4f8544
Aayush1Adk <aayush.adhikari2076@gmail.com>
2026-07-20 16:06:09 +0545

    small error

--- Backend/src/app.js
+++ Backend/src/app.js
@@ -10,7 +10,7 @@ app.use(
     cors({
     origin: [
     "http://localhost:5173",
-    "https://music-app-one-puce.vercel.app/"
+    "https://music-app-one-puce.vercel.app"
     ],
     credentials: true,
     })
